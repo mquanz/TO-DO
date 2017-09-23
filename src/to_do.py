f5fe416d9601d20576c2fc28062a7bbb97df6dda
Marterminator <martin.quanz@gmail.com>
2017-09-23 17:50:07 +0200

    added insert and print function

--- src/to_do.py
+++ src/to_do.py
@@ -1,1 +1,19 @@
-print('Hello World.')
+task_list = []
+
+def insert_elements(task):
+    task_list.append(task)
+
+def print_elements():
+    number = 0
+    for task in task_list:
+        number = number + 1
+        print(number,task)
+
+#Test
+insert_elements('Aufstehen')
+insert_elements('Duschen')
+insert_elements('Frühstück')
+insert_elements('Arbeit')
+insert_elements('Coden')
+insert_elements('Koten')
+print_elements()
