e6da3e7327c73901397421ab6df7a1bb6dfe347d
Marterminator <martin.quanz@gmail.com>
2017-11-05 18:23:45 +0100

    completed class TaskList

--- src/functions.py
+++ src/functions.py
@@ -24,66 +24,36 @@ class TaskList:
             else:
                 status = '[ ]'
             print(number, status, task.description)
+
+    def delete_task(self, number):
+        self.task_list.pop(number-1)
+
+    def serialize(self):
+        dicts = [task.__dict__ for task in self.task_list]
+        return json.dumps(dicts)
+
+    def deserialize(self, json_string):
+        dicts = json.loads(json_string)
+        tasks = []
+        for dic in dicts:
+            tasks.append(Task(dic['description'],dic['done']))
+        self.task_list = tasks
+
+    def export(self):
+        obj = open(FILENAME, 'wb')
+        obj.write(self.serialize().encode('utf-8'))
+        obj.close
+
+    def importe(self):
+        obj = open(FILENAME, 'r')
+        self.deserialize(obj.read())
+        obj.close
+
+    def clear_list(self):
+        self.task_list = []
         
 class Task:
 
     def __init__(self, description, done = False):
         self.description = description
         self.done = done
-
-
-#Test
-
-#Create instance of TaskList
-task_list1 = TaskList()
-
-#Insert element and print
-task_list1.insert_task('Test')
-
-print(task_list1.task_list[0].description)
-
-#Mark done element and print
-task_list1.mark_done(1)
-
-print(task_list1.task_list[0].done)
-
-#Print
-task_list1.print_list()
-
-
-#def print_elements():
-#    number = 0
-#    for task in task_list:
-#        number = number + 1
-#        if task.done:
-#            status = '[x]'
-#        else:
-#            status = '[ ]'
-#        print(number,status,task.description)
-
-def delete_element(number):
-    task_list.pop(number - 1)
-
-def serialize():
-    dicts = [task.__dict__ for task in task_list]
-    return json.dumps(dicts)
-
-def deserialize(json_string):
-    dicts = json.loads(json_string)
-    tasks = []
-    for dic in dicts:
-        tasks.append(Task(dic['description'],dic['done']))
-    task_list[:] = tasks
-
-def export():
-    obj = open(FILENAME, 'wb')
-    obj.write(serialize().encode('utf-8'))
-    obj.close
-
-def importe():
-    obj = open(FILENAME, 'r')
-    deserialize(obj.read())
-    obj.close
-
-def clear_list():
-    task_list[:] = []
