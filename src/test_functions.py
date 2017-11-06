599f93a1b3ab9baa9749160b174b4a6f74d81929
Marterminator <martin.quanz@gmail.com>
2017-11-06 21:45:50 +0100

    created set up for test and added mark_done test

--- src/test_functions.py
+++ src/test_functions.py
@@ -3,10 +3,26 @@ from functions import *
 
 class TestFunctions(unittest.TestCase):
 
+    def setUp(self):
+        self.task_list1 = TaskList()
+        self.task_list1.insert_task('Das')
+        self.task_list1.insert_task('ist')
+        self.task_list1.insert_task('ein')
+        self.task_list1.insert_task('Test')
+        self.task_list1.insert_task('Ha')
+        self.task_list1.insert_task('Ha!')
+
+    def tearDown(self):
+        pass
+
     def test_insert_task(self):
-        task_list1 = TaskList()
-        task_list1.insert_task('Test')
-        self.assertEqual(task_list1.task_list[0].description, 'Test')
+        self.assertEqual(self.task_list1.task_list[0].description, 'Das')
+        self.assertEqual(self.task_list1.task_list[1].description, 'ist')
+
+    def test_mark_done(self):
+        self.task_list1.mark_done(1)
+        self.assertTrue(self.task_list1.task_list[0].done)
+
 
 if __name__ == '__main__':
     unittest.main()
