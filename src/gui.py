f9767c57ef892f80ab7aabae71b9bf6a2948f71d
Marterminator <martin.quanz@gmail.com>
2017-11-11 16:28:11 +0100

    created class for main window and added exit button

--- src/gui.py
+++ src/gui.py
@@ -1,13 +1,20 @@
-import tkinter
+from tkinter import *
 
-root = tkinter.Tk()
 
-class main():
-    def __init__(self):
-        rahmen = tkinter.Frame()
-        text = tkinter.Label(rahmen, text='Welcome to TO-DO-List!')
-        text.pack()
-        rahmen.pack()
-     
-main()
+class Window:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.label1 = Label(frame, text = 'Welcome to TO-DO-List', bg = 'black', fg = 'white')
+        self.label1.grid(row = 0)
+
+        self.exit_button = Button(frame, text = 'Exit', command = frame.quit)
+        self.exit_button.grid(row = 1) 
+
+
+root = Tk()
+create_window = Window(root)
 root.mainloop()
