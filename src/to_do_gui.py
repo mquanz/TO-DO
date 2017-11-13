90d025ce9dd8fe71576389721668a301fd121f2f
Marterminator <martin.quanz@gmail.com>
2017-11-13 11:47:50 +0100

    added delete feature to gui

--- src/to_do_gui.py
+++ src/to_do_gui.py
@@ -16,21 +16,33 @@ class Window:
         self.add_entry = Entry(master) 
         self.add_entry.grid(row = 1)
 
-        self.add_button = Button(master, text = 'ADD')
+        self.add_button = Button(master, text = 'ADD', bg = 'green')
         self.add_button.bind('<Button-1>', self.add_task)
         self.add_button.grid(row = 1, column = 1)
 
+        self.del_entry = Entry(master) 
+        self.del_entry.grid(row = 2)
+
+        self.del_button = Button(master, text = 'DELETE', bg = 'red')
+        self.del_button.bind('<Button-1>', self.del_task)
+        self.del_button.grid(row = 2, column = 1)
+
         self.print_button = Button(master, text = 'PRINT')
         self.print_button.bind('<Button-1>', self.print_tasks)
-        self.print_button.grid(row = 2)
+        self.print_button.grid(row = 3)
 
         self.exit_button = Button(master, text = 'EXIT', command = master.quit)
-        self.exit_button.grid(row = 3) 
+        self.exit_button.grid(row = 4) 
 
     def add_task(self, arg):
         user_input = self.add_entry.get()
         task_list1.insert_task(user_input)
         self.add_entry.delete(0, END)
+
+    def del_task(self, arg):
+        user_input = int(self.del_entry.get())
+        task_list1.delete_task(user_input)
+        self.del_entry.delete(0, END)
 
     def print_tasks(self, arg):
         output_list = task_list1.print_list()
