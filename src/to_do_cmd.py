a1b539854d0f322639aa85aa7ec0ba9530a5c8a6
Marterminator <martin.quanz@gmail.com>
2017-12-05 18:04:27 +0100

    changed separator in cmd app

--- src/to_do_cmd.py
+++ src/to_do_cmd.py
@@ -2,10 +2,10 @@ from functions import *
     
 print('Welcome to TO-DO-List!\n')
 print('Commands:')
-print('"add: <element>": Add element to the TO-DO-List')
+print('"add <element>": Add element to the TO-DO-List')
 print('"print": Print the TO-DO-List')
-print('"delete: <number>": Delete an element')
-print('"mark: <number>": Mark done element')
+print('"delete <number>": Delete an element')
+print('"mark <number>": Mark done element')
 print('"export": Export list in data.txt')
 print('"import": Import list from data.txt')
 print('"clear": Clear list')
@@ -17,26 +17,17 @@ print('\nWith starting this program you just created a new list at ' + my_list.c
 
 while True:
     user_input = input('> ')
-    position = user_input.find(':')
-    if user_input[0:position] == 'add':
-        position = position + 1
-        while user_input[position] == ' ':
-            position = position + 1
-        my_list.insert_task(user_input[position:])
-    elif user_input[0:position] == 'delete':
-        position = position + 1
-        while user_input[position] == ' ':
-            position = position + 1
-        my_list.delete_task(int(user_input[position:]))
+    user_list = user_input.split(' ')
+    if user_list[0] == 'add':
+        my_list.insert_task(user_list[1])
+    elif user_list[0] == 'delete':
+        my_list.delete_task(int(user_list[1]))
     elif user_input == 'print':
         output_list = show_list(my_list)
         for task in output_list:
             print(task)        
-    elif user_input[0:position] == 'mark':
-        position = position + 1
-        while user_input[position] == ' ':
-            position = position + 1
-        my_list.mark_done(int(user_input[position:]))
+    elif user_list[0] == 'mark':
+        my_list.mark_done(int(user_list[1]))
     elif user_input == 'export':
         export(my_list)
     elif user_input == 'import':
